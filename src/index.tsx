48b5a3e9efa9046a59f4d6c4bca8c4d9a7f12d9a
Nenad <nenad.bugaric@esome.com>
2020-07-22 16:30:15 +0200

    basic demo for mataterial-ui

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 
 import ReactTable from './tables/react-table'
 import MaterialTable from "./tables/material-table";
+import MaterialUITable from "./tables/material-ui-table";
 
 ReactDOM.render(
   <React.StrictMode>
     <ReactTable />
     <hr />
+
+    <MaterialUITable />
+    <hr />
+
     <MaterialTable />
   </React.StrictMode>,
   document.getElementById('root')
