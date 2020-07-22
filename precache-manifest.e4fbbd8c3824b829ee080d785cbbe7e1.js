d43cfc55a220e899a962fc74f61de9afcd304c1a
Nenad <nenad.bugaric@esome.com>
2020-07-22 17:32:27 +0200

    Updates

---
+++ precache-manifest.e4fbbd8c3824b829ee080d785cbbe7e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3487a24d07447ec92eada3c898223f57",
+    "url": "/tables/index.html"
+  },
+  {
+    "revision": "8a2ed64b443ba0139ed0",
+    "url": "/tables/static/js/2.a95d119d.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/tables/static/js/2.a95d119d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a43c7902e3feecea28",
+    "url": "/tables/static/js/main.3da49f0b.chunk.js"
+  },
+  {
+    "revision": "b3b52be975aba5fcbaf0",
+    "url": "/tables/static/js/runtime-main.8b434a37.js"
+  }
+]);
